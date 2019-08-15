4ee9935398551231ae53cb37369139ec5c80c2f4
gauravpatil29 <gaurav@plmlogix.com>
2019-08-16 00:46:56 +0530

    added error screen-view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  status: Status = Status.LOADING;
+  status: Status = Status.ERROR;
   error: String;
 }
 
