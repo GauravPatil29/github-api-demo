c9b3ba77a7dfabe72fac23666d8ef65503a849bb
Gaurav Patil <gauravpatil2994@gmail.com>
2020-07-07 16:43:50 +0530

    Firebase deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
 import { DataService } from './providers/data.service';
 import { UserComponent } from './components/user/user.component';
-import { FormsModule } from '@angular/forms';
 import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
